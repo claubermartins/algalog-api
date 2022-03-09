75e37be4b90535ecdc6969b2698cc68a43f45399
Clauber Martins Pinto <clauber.pinto@mapa.magov.intranet>
2022-03-09 16:27:21 -0300

    Validando com Bean Validation

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -3,6 +3,8 @@ package com.algaworks.algalog.api.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -46,13 +48,13 @@ public class ClienteController {
 	
 	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping
-	public Cliente adicionar(@RequestBody Cliente cliente) {
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
 	
 	@PutMapping("/{clienteId}")
 	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,
-			@RequestBody Cliente cliente){
+			@Valid @RequestBody Cliente cliente){
 		if(!clienteRepository.existsById(clienteId)) {
 			return ResponseEntity.notFound().build();
 		}
